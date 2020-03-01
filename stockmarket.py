4b551335d08a5da452a4c94dd47af316102b4ecc
gaunah <christoph@zelinski.de>
2020-03-01 21:22:51 +0100

    save data to disk and use as source if not older than an hour

--- stockmarket.py
+++ stockmarket.py
@@ -2,56 +2,82 @@ import pandas as pd
 from alpha_vantage.techindicators import TechIndicators
 from alpha_vantage.timeseries import TimeSeries
 from matplotlib import pyplot as plt
+from datetime import datetime
+import os
 
-with open("./api-key.txt", "r") as keyfile:
-    api_key = keyfile.readline().strip()
+try:
+    with open("./api-key.txt", "r") as keyfile:
+        api_key = keyfile.readline().strip()
+except FileNotFoundError as e:
+    print(e)
 
-# ts = TimeSeries(key=api_key, output_format="pandas")
-ti = TechIndicators(key=api_key, output_format="pandas")
 
 symbol = input("Enter stock ticker: ").upper()
+date = datetime.now()
+filename = "weeklyData_" + symbol + date.strftime("_%Y-%m-%d_%H") + ".zip"
 
-# get PriceData
-# weekly_data, meta_data = ts.get_weekly_adjusted(symbol)
-# del weekly_data["7. dividend amount"]
-# weekly_data = weekly_data.sort_index()  # sort to newst date last
+# try to get saved data (if not older than an hour)
+oldData = True
+try:
+    weekly_data = pd.read_pickle("./data/" + filename)
+    print("Use {} as datasource.".format(filename))
+except FileNotFoundError:
+    print("Fetch new data...")
+    oldData = False
 
-# get MACD
-macd_weekly, macd_meta = ti.get_macd(symbol, interval="weekly",
-                                     fastperiod=12, slowperiod=26, signalperiod=9)
-macd_weekly = macd_weekly.sort_index()
+if not oldData:
+    # ts = TimeSeries(key=api_key, output_format="pandas")
+    ti = TechIndicators(key=api_key, output_format="pandas")
 
-# get EMA
-ema13_weekly, ema13_meta = ti.get_ema(symbol, interval="weekly",
-                                      time_period=13)
-ema13_weekly = ema13_weekly.sort_index()
+    # get PriceData
+    # weekly_data, meta_data = ts.get_weekly_adjusted(symbol)
+    # del weekly_data["7. dividend amount"]
+    # weekly_data = weekly_data.sort_index()  # sort to newst date last
 
-# join everything
-weekly_data = macd_weekly.join(ema13_weekly)
+    # get MACD
+    macd_weekly, macd_meta = ti.get_macd(symbol, interval="weekly",
+                                         fastperiod=12, slowperiod=26, signalperiod=9)
+    macd_weekly = macd_weekly.sort_index()
 
+    # get EMA
+    ema13_weekly, ema13_meta = ti.get_ema(symbol, interval="weekly",
+                                          time_period=13)
+    ema13_weekly = ema13_weekly.sort_index()
 
-def elder_impulse(idx):
-    if idx == 0:
-        return 0
+    # join everything
+    weekly_data = macd_weekly.join(ema13_weekly)
 
-    emaCur = weekly_data["EMA"].iat[idx]
-    emaLast = weekly_data["EMA"].iat[idx-1]
-    mhCur = weekly_data["MACD_Hist"].iat[idx]
-    mhLast = weekly_data["MACD_Hist"].iat[idx-1]
+    def elder_impulse(idx):
+        if idx == 0:
+            return 0
 
-    elder_bulls = (emaCur > emaLast) and (mhCur > mhLast)
-    elder_bears = (emaCur < emaLast) and (mhCur < mhLast)
-    # red = -1
-    # blue = 0
-    # green = 1
-    elder_color = 1 if elder_bulls else (
-        -1 if elder_bears else 0)
-    return elder_color
+        emaCur = weekly_data["EMA"].iat[idx]
+        emaLast = weekly_data["EMA"].iat[idx-1]
+        mhCur = weekly_data["MACD_Hist"].iat[idx]
+        mhLast = weekly_data["MACD_Hist"].iat[idx-1]
 
+        elder_bulls = (emaCur > emaLast) and (mhCur > mhLast)
+        elder_bears = (emaCur < emaLast) and (mhCur < mhLast)
+        # red = -1
+        # blue = 0
+        # green = 1
+        elder_color = 1 if elder_bulls else (
+            -1 if elder_bears else 0)
+        return elder_color
 
-colormap = {-1: "red", 0: "blue", 1: "green"}
-# add Elder Impulse
-lenght = len(weekly_data.index)
-weekly_data["Elder Impulse"] = [colormap[elder_impulse(i)] for i in range(lenght)]
+    colormap = {-1: "red", 0: "blue", 1: "green"}
+    # add Elder Impulse
+    lenght = len(weekly_data.index)
+    print("Calc elder impulse...")
+    weekly_data["Elder Impulse"] = [
+        colormap[elder_impulse(i)] for i in range(lenght)]
 
+    # save data
+    try:
+        os.mkdir("./data")
+    except FileExistsError:
+        pass
+    weekly_data.to_pickle("./data/" + filename)
+
+print()
 print(weekly_data.tail())
